036ef9136163ae114e182626c65538e522a92bd1
oliverqueencc <441633565@qq.com>
2018-02-25 02:09:01 +0800

    matplotlib

--- GUI/image.py
+++ GUI/image.py
@@ -7,14 +7,6 @@ Created on Sun Feb 25 01:37:56 2018
 """
 import numpy as np
 import cv2 as cv
-
-img = cv.imread('2.jpg',0)
-cv.namedWindow('image',cv.WINDOW_NORMAL)
-cv.imshow('image',img)
-cv.waitKey(0)
-cv.destroyAllWindows()"""
-import numpy as np
-import cv2 as cv
 img = cv.imread('2.jpg',0)
 cv.imshow('image',img)
 k = cv.waitKey(0)
@@ -22,4 +14,11 @@ if k == 27:         # wait for ESC key to exit
     cv.destroyAllWindows()
 elif k == ord('s'): # wait for 's' key to save and exit
     cv.imwrite('messigray.png',img)
-    cv.destroyAllWindows()
+    cv.destroyAllWindows()"""
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('2.jpg',0)
+plt.imshow(img,cmap='gray',interpolation='bicubic')
+plt.xticks([]),plt.yticks([])
+plt.show
